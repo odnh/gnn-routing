29989b559abaafe4180af59385fdf7a25157f73b
Oliver Hope <oh260@cl.cam.ac.uk>
2020-02-13 11:55:58 +0000

    flesh out ddr env skeleton

--- gym-gnn/gym_gnn/envs/ddr_env.py
+++ gym-gnn/gym_gnn/envs/ddr_env.py
@@ -10,18 +10,56 @@ from gym.utils import seeding
 class DDREnv(gym.Env):
     """
     Gym env for data driven routing
-    Observations are:
-    Actions are:
-    Rewards are:
+    Observations are: last k routing and DMs
+    Actions are: a routing (standard version: dest splitting ratios, softmin version: edge weigths)
+    Rewards are: utilisation compared to maximum utilisation
     """
 
-    def __init__(self):
-        pass
-    def step(self, action):
-        pass
-    def reset(self):
-        pass
+    def __init__(self, dm_generator: Callable[[],Generator[np.ndarray]], dm_memory_length: int, graph: nerworkx.Graph):
+        self.dm_generator_getter = dm_generator_getter
+        self.dm_generator = dm_generator_getter()
+        self.dm_memory_length = dm_memory_length
+        self.dm_memory = []
+        self.graph = graph
+
+    def step(self, action: routing) -> Tuple[List[np.ndarray], float, bool, Dict[]]:
+        # update dm and history
+        new_dm = next(dm_generator)
+        self.dm_memory.append(new_dm)
+        if (len(self.dm_memory_length) > self.dm_memory_length):
+            self.dm_memory.pop(0)
+        routing = get_routing(action)
+        reward = get_reward(routing)
+        # work out when to set done
+        return (self.dm_memory.copy(), reward, False, dict())
+
+    def reset(self) -> List[np.ndarray]:
+        self.dm_generator = self.dm_generator_getter()
+        self.dm_memory = [next(self.dm_generator)]
+        return self.dm_memory.copy()
+
     def render(self, mode='human'):
         pass
+
     def close(self):
         pass
+
+    def get_reward(routing) -> float:
+        utilisation = calc_utilisation(self.graph, self.dm_memory[0], routing)
+        opt_utilisation = calc_opt_utilisation(self.graph, self.dm_memory[0])
+        return -(utilisation/opt_utilisation)
+
+    def calc_opt_utilisation(graph, dm):
+        pass
+
+class DDREnvDestSplitting(DDREnv):
+    def get_routing(splitting_ratios) -> routing:
+        pass
+
+class DDREnvSoftmin(DDREnv):
+    def get_routing(edge_weights) -> routing:
+        pass
+
+# Need to add
+#  1. Sample dm generator
+#  2. Function to calc OPT and u
--- rlpyt_experiments/experiments/train_cartpole.py
+++ rlpyt_experiments/experiments/train_cartpole.py
@@ -16,17 +16,17 @@ def build_and_train(env_id="CartPole-v0", run_ID=0, cuda_idx=None):
         EnvCls=gym_make,
         env_kwargs=dict(id=env_id),
         eval_env_kwargs=dict(id=env_id),
-        batch_T=6,
-        batch_B=2,
-        max_decorrelation_steps=0,
+        batch_T=8,
+        batch_B=4,
+        max_decorrelation_steps=2,
     )
     algo = PPO()
-    agent = CartPolePgAgent()#CartPoleGcnAgent()
+    agent = CartPoleGcnAgent()#CartPolePgAgent()
     runner = MinibatchRl(
         algo=algo,
         agent=agent,
         sampler=sampler,
-        n_steps=60000,
+        n_steps=80000,
         log_interval_steps=1e3,
         affinity=affinity,
     )
