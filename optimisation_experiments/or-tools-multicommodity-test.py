d03774dd447e6165f12b37ad367ac1d43ee674c6
Oliver Hope <oliverdnhope@gmail.com>
2020-03-04 10:36:34 +0000

    complete working optimiser

--- optimisation_experiments/or-tools-multicommodity-test.py
+++ optimisation_experiments/or-tools-multicommodity-test.py
@@ -3,21 +3,12 @@ import numpy as np
 import networkx as nx
 from ortools.linear_solver import pywraplp
 
-#NB: makes assumption that graph is bidirectional (i.e. is directed but always has an edge the same width both ways)
-
 def main():
-    # num_nodes = 10
-    # # create/get graph
-    # graph = nx.gnp_random_graph(num_nodes, 0.1).to_directed()
-    # # generate & add capacities
-    # for edge in graph.edges():
-    #     capacity = 12 #random.randint(1,10)
-    #     graph[edge[0]][edge[1]]['weight'] = capacity
-    #     graph[edge[1]][edge[0]]['weight'] = capacity
-
+    ## BUILD SIMPLE GRAPH FOR TESTING
     num_nodes = 4
+    # Assumes graph always has edge in both directions even though `directed`
     graph = nx.Graph().to_directed()
-    edge_weight = 100
+    edge_weight = 10
     graph.add_edge(0, 1, weight=edge_weight)
     graph.add_edge(1, 0, weight=edge_weight)
     graph.add_edge(1, 2, weight=edge_weight)
@@ -26,13 +17,13 @@ def main():
     graph.add_edge(3, 2, weight=edge_weight)
     graph.add_edge(3, 0, weight=edge_weight)
     graph.add_edge(0, 3, weight=edge_weight)
+    demands = np.ones(shape=(num_nodes, num_nodes), dtype=float)
 
+
+    ## ACTUAL CODE START
     edges = list(graph.edges())
     edge_index_dict = {edge : i for i, edge in enumerate(edges)}
-    # create demands
-    # demands = np.random.randint(0, high=10, size=(num_nodes,
-    #                             num_nodes)).astype(float)
-    demands = np.ones(shape=(num_nodes, num_nodes), dtype=float)
+    
     # create commodities from demands (make sure to ignore demand to self)
     commodities = [(i, j, demands[i, j]) for i in range(num_nodes)
                                          for j in range(num_nodes) if i != j]
@@ -49,7 +40,8 @@ def main():
     for i in range(len(commodities)):
         flow_variable_edges = []
         for j in range(graph.number_of_edges()):
-            flow_variable_edges.append(solver.NumVar(0, 1, '({},{})'.format(i, j)))
+            flow_variable_edges.append(
+                solver.NumVar(0, 1, '({},{})'.format(i, j)))
         flow_variables.append(flow_variable_edges)
 
     print('Number of variables =', solver.NumVariables())
@@ -59,8 +51,8 @@ def main():
     capacity_constraints = []
     for i, edge in enumerate(graph.edges()):
         # Constraint between 0 and edge capacity
-        constraint_i = solver.Constraint(0, graph.get_edge_data(*edge)['weight'],
-                                         '(1,{},{})'.format(*edge))
+        constraint_i = solver.Constraint(
+            0, graph.get_edge_data(*edge)['weight'],'(1,{},{})'.format(*edge))
         for j, commodity in enumerate(commodities):
             # Coefficient for jth flow over ith edge is scaled by flow width
             constraint_i.SetCoefficient(flow_variables[j][i],
@@ -77,9 +69,11 @@ def main():
                 constraint_j = solver.Constraint(0, 0, '(2,{},{})'.format(i, j))
                 for k in list(graph.adj[j].keys()):
                     # Ingress edges
-                    constraint_j.SetCoefficient(flow_variables[i][edge_index_dict[(k, j)]], 1)
+                    constraint_j.SetCoefficient(
+                        flow_variables[i][edge_index_dict[(k, j)]], 1)
                     # Egress edges
-                    constraint_j.SetCoefficient(flow_variables[i][edge_index_dict[(j, k)]], -1)
+                    constraint_j.SetCoefficient(
+                        flow_variables[i][edge_index_dict[(j, k)]], -1)
                 constraints_flow_i.append(constraint_j)
         conservation_transit_constraints.append(constraints_flow_i)
 
@@ -90,9 +84,11 @@ def main():
         constraint_i = solver.Constraint(1, 1, '(3,{})'.format(i))
         for edge_dest in list(graph.adj[commodity[0]].keys()):
             constraint_i.SetCoefficient(
-                flow_variables[i][edge_index_dict[(commodity[0], edge_dest)]], 1)
+                flow_variables[i][edge_index_dict[(commodity[0], edge_dest)]],
+                1)
             constraint_i.SetCoefficient(
-                flow_variables[i][edge_index_dict[(edge_dest, commodity[0])]], -1)
+                flow_variables[i][edge_index_dict[(edge_dest, commodity[0])]],
+                -1)
         conservation_source_constraints.append(constraint_i)
 
     # Conservation of flow at destination node
@@ -111,13 +107,26 @@ def main():
 
     ## OBJECTIVES
     # Implementation of the load-balancing example from wikipedia
+    # First we add more constraints so that we are minimising the maximum
+    max_utilisation_variable = solver.NumVar(0, solver.Infinity(),
+                                             'max_link_utilisation')
+    min_of_max_constraints = []
+    for i, edge in enumerate(graph.edges()):
+        # Constraint that '-inf < f_0 + f_1 +... - max < 0'
+        # i.e 'f_0 + f_1 + ... < max'
+        constraint_i = solver.Constraint(-solver.Infinity(), 0,
+                                         '(5,{})'.format(i))
+        constraint_i.SetCoefficient(max_utilisation_variable, -1)
+        for j, flow_variable in enumerate(flow_variables):
+            constraint_i.SetCoefficient(flow_variable[i],
+                                        commodities[j][2] /
+                                        graph.get_edge_data(*edge)['weight'])
+        min_of_max_constraints.append(constraint_i)
+
+    # Objective now is to minimise the maximum link utilisation
     objective = solver.Objective()
-
-    for i, flow_variable in enumerate(flow_variables):
-        for flow_variable_edge in flow_variable:
-            objective.SetCoefficient(flow_variable_edge, 1)
+    objective.SetCoefficient(max_utilisation_variable, 1)
     objective.SetMinimization()
-
     solver.Solve()
 
     print('Solution:')
