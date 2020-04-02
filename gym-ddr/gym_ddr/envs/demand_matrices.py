cdeeec9f5082150ff3d949c280f226de213d926f
Oliver Hope <oliverdnhope@gmail.com>
2020-04-02 12:28:19 +0100

    Add gravity demand generation

--- gym-ddr/gym_ddr/envs/demand_matrices.py
+++ gym-ddr/gym_ddr/envs/demand_matrices.py
@@ -1,10 +1,10 @@
 from typing import Callable, Type, Iterator
+
 import networkx as nx
 import numpy as np
 from numpy.random import RandomState
 
 Demand = Type[np.ndarray]
-
 
 # NB: demands are not matrices, but 1D arrays in deterministic ordering
 
@@ -18,6 +18,9 @@ def sparsify(
     Args:
       demands: demand matrix
       sparsity: between 0 and 1
+      random_state: a numpy RandomState to control seed
+    Returns:
+        A sparsified demand array
     """
     sparsified_demands = demands.copy()
     for i in range(len(demands)):
@@ -26,13 +29,30 @@ def sparsify(
     return sparsified_demands
 
 
-def gravity_demand(number_of_flows: int, graph: nx.Graph) -> Demand:
+def gravity_demand(graph: nx.DiGraph) -> Demand:
     """
     Generates gravity demand (deterministic, based on bandwidth) for one time
     step
+    Args:
+        graph: Networkx DiGraph with 'weight' on edges to generate demand from
+    Returns:
+        A demand array
     """
-    # TODO: implement
-    pass
+    num_nodes = graph.number_of_nodes()
+    sorted_edges = sorted(graph.edges(data=True))
+    edge_weights = [e[2]['weight'] for e in sorted_edges]
+
+    total_flow = sum(edge_weights)
+    node_in_flow = np.zeros(num_nodes, np.float32)
+    node_out_flow = np.zeros(num_nodes, np.float32)
+
+    for i, edge in enumerate(sorted_edges):
+        node_in_flow[edge[1]] += edge_weights[i]
+        node_out_flow[edge[0]] += edge_weights[i]
+
+    return np.divide(np.array(
+        [node_out_flow[i] * node_in_flow[j] for i in range(num_nodes) for j in
+         range(num_nodes) if i != j]), total_flow)
 
 
 def bimodal_demand(
@@ -40,6 +60,11 @@ def bimodal_demand(
         random_state: RandomState = RandomState()) -> Demand:
     """
     Generates bimodal demand (probabilistic) for one time step
+    Args:
+        number_of_flows: the number of flows to create
+        random_state: numpy RandomState (for seeding)
+    Returns:
+        A demand array
     """
     demand = np.zeros(number_of_flows, dtype=float)
     for i in range(number_of_flows):
@@ -72,7 +97,6 @@ def cyclical_sequence(
     Returns:
       sequence as an iterator
     """
-    demand = demand_generator()
     short_sequence = [sparsify(demand_generator(), sparsity, random_state) for _
                       in range(q)]
     i = 0
