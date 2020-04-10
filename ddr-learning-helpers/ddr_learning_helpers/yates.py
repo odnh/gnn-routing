fix all the utilisation calculation and partially fix softmin stuff. Big issue is it no longer works at all. Find out why...

ddr-learning-helpers/ddr_learning_helpers/yates.py
@@ -33,6 +33,19 @@ def nx_to_yates_dot(graph: nx.DiGraph, path: str):
     nx.drawing.nx_pydot.write_dot(g, path)
 
 
+def scale_per_node(routing: np.ndarray, graph: nx.DiGraph) -> np.ndarray:
+    """
+    Takes a routing an then normalises the values for the out edges from each
+    node
+    Args:
+        routing: a routing
+        graph: the graph for edge data
+    Returns:
+        a normalised routing
+    """
+    # TODO: implement
+
+
 def get_ddr_routing(raw_routing: str, graph: nx.DiGraph) -> np.ndarray:
     """
     Reads the raw output from the routing given by Yates and puts it into the
@@ -59,24 +72,23 @@ def get_ddr_routing(raw_routing: str, graph: nx.DiGraph) -> np.ndarray:
     match_flow_line = re.compile('->')
     match_edges_line = re.compile('@')
     match_src_dst = re.compile(r'\d+')
-    match_edges = re.compile(r'\d+(?=,|\))')
+    match_edges = re.compile(r'\(s\d+,s\d+\)')
+    match_edge_ends = re.compile(r'\d+')
     match_value = re.compile(r'\d.\d+')
 
     lines = raw_routing.split("\n")
-
     for line in lines:
         if match_flow_line.search(line) is not None:
             src_dst = match_src_dst.findall(line)
             current_flow = (int(src_dst[0]), int(src_dst[1]))
         elif match_edges_line.search(line) is not None:
             edges = match_edges.findall(line)
+            edge_ends = [match_edge_ends.findall(edge) for edge in edges]
             value = match_value.findall(line)
-            for i in range(0, len(edges), 2):
-                # set routing
-                edge = (int(edges[i]), int(edges[i + 1]))
-                if edge[0] == edge[1]: continue
-                routing[flow_map[current_flow]][edge_map[edge]] = float(
-                    value[0])
+            for edge_str in edge_ends:
+                edge = (int(edge_str[0]), int(edge_str[1]))
+                if edge[0] != edge[1]:
+                    routing[flow_map[current_flow]][edge_map[edge]] += float(value[0])
 
     return routing
 
@@ -100,6 +112,7 @@ def get_oblivious_routing(graph: nx.DiGraph) -> Routing:
     raw_routing = subprocess.run([raeke_path, oblivious_tmp_path],
                                  stdout=subprocess.PIPE).stdout.decode('utf-8')
     ddr_routing = get_ddr_routing(raw_routing, graph)
+    normalised_routing = scale_per_node(ddr_routing, graph)
     os.remove(oblivious_tmp_path)
 
-    return ddr_routing
+    return normalised_routing

gym-ddr/gym_ddr/envs/ddr_env.py
@@ -1,4 +1,3 @@
-import itertools
 from typing import Tuple, List, Dict, Callable, Generator, Type
 
 import gym
@@ -8,10 +7,10 @@ from scipy.special import softmax
 
 from . import max_link_utilisation
 
-Routing = Type[np.ndarray]
-Demand = Type[np.ndarray]
-Observation = Type[np.ndarray]
-Action = Type[np.ndarray]
+Routing = np.ndarray
+Demand = np.ndarray
+Observation = np.ndarray
+Action = np.ndarray
 
 
 class DDREnv(gym.Env):
@@ -32,13 +31,15 @@ class DDREnv(gym.Env):
                      [],
                      Generator[Demand, None, None]],
                  dm_memory_length: int,
-                 graph: nx.DiGraph):
+                 graph: nx.DiGraph,
+                 oblivious_routing: np.ndarray = None):
         """
         Args:
           dm_generator_getter: a function that returns a generator for demand
                                matrices (so can reset)
           dm_memory_length: the length of the dm history we should train on
           graph: the graph we will be routing over
+          oblivious_routing: an oblivious routing
         """
         self.dm_generator_getter = dm_generator_getter
         self.dm_generator = dm_generator_getter()
@@ -60,11 +61,14 @@ class DDREnv(gym.Env):
                    (graph.number_of_nodes() - 1),),
             dtype=np.float64)
 
-        self.oblivious_routing = max_link_utilisation.racke_routing(graph)
+        self.oblivious_routing = oblivious_routing
 
-    def step(self, action: Type[np.ndarray]) -> Tuple[Observation,
-                                                      float,
-                                                      bool, Dict[None, None]]:
+        self.opt_utilisation = 0.0
+        self.utilisation = 0.0
+
+    def step(self, action: np.ndarray) -> Tuple[Observation,
+                                                float,
+                                                bool, Dict[None, None]]:
         """
         Args:
           action: a routing this is a fully specified routing (must be 1D
@@ -119,11 +123,11 @@ class DDREnv(gym.Env):
         Reward calculated as utilisation of graph given routing compared to
         optimal. May have to call external libraries to calculate efficiently.
         """
-        utilisation = max_link_utilisation.calc(self.graph, self.dm_memory[0],
+        self.utilisation = max_link_utilisation.calc(self.graph, self.dm_memory[0],
                                                 routing)
-        opt_utilisation = max_link_utilisation.opt(self.graph,
+        self.opt_utilisation = max_link_utilisation.opt(self.graph,
                                                    self.dm_memory[0])
-        return -(utilisation / opt_utilisation)
+        return -(self.utilisation / self.opt_utilisation)
 
     def get_observation(self) -> Observation:
         """
@@ -138,11 +142,15 @@ class DDREnv(gym.Env):
         Gets a data dict to return in the step function. Contains the
         utilisation under oblivious routing
         Returns:
-            dict with utilisation under oblivious routing
+            dict with utilisation under oblivious routing, optimal and action
         """
-        data_dict = dict()
-        data_dict['oblivious_utilisation'] = max_link_utilisation.calc(
-            self.graph, self.dm_memory[0], self.oblivious_routing)
+        data_dict = {
+            'utilisation': self.utilisation,
+            'opt_utilisation': self.opt_utilisation
+        }
+        if self.oblivious_routing is not None:
+            data_dict['oblivious_utilisation'] = max_link_utilisation.calc(
+                self.graph, self.dm_memory[0], self.oblivious_routing)
         return data_dict
 
 
@@ -158,8 +166,8 @@ class DDREnvDest(DDREnv):
                      [],
                      Generator[Demand, None, None]],
                  dm_memory_length: int,
-                 graph: nx.DiGraph):
-        super().__init__(dm_generator_getter, dm_memory_length, graph)
+                 graph: nx.DiGraph, **kwargs):
+        super().__init__(dm_generator_getter, dm_memory_length, graph, **kwargs)
 
         # For calculating the action space size and routing translation
         # sorted to match encoding ordering of nodes
@@ -240,8 +248,8 @@ class DDREnvSoftmin(DDREnv):
                      Generator[Demand, None, None]],
                  dm_memory_length: int,
                  graph: nx.DiGraph,
-                 gamma: float = 2):
-        super().__init__(dm_generator_getter, dm_memory_length, graph)
+                 gamma: float = 2, **kwargs):
+        super().__init__(dm_generator_getter, dm_memory_length, graph, **kwargs)
 
         # Precompute list of flows for use in routing translation
         num_nodes = self.graph.number_of_nodes()
@@ -250,16 +258,16 @@ class DDREnvSoftmin(DDREnv):
         self.gamma = gamma
 
         self.action_space = gym.spaces.Box(
-            low=-1.0,
-            high=1.0,
+            low=-np.inf,
+            high=np.inf,
             shape=(graph.number_of_edges(),),
             dtype=np.float64)
 
         # Indices of the edges for lookup in routing translation
         # Takes each edge of graph to an index under its source node
         self.edge_index = {}
+        count = 0
         for node in range(graph.number_of_nodes()):
-            count = 0
             for edge in sorted(graph.out_edges(node)):
                 self.edge_index[edge] = count
                 count += 1
@@ -287,7 +295,9 @@ class DDREnvSoftmin(DDREnv):
         for i, flow in enumerate(self.flows):
             for j, edge in enumerate(sorted(self.graph.edges())):
                 full_routing[i][j] = softmin_edge_weights[j]  # Surely wrong?
-
+        # with np.printoptions(threshold=np.inf):
+        #     print(action)
+        #     print(full_routing)
         return full_routing
 
     def softmin(self, array: List[float]) -> List[float]:
@@ -300,7 +310,7 @@ class DDREnvSoftmin(DDREnv):
         """
         exponentiated = [np.exp(-self.gamma * i) for i in array]
         total = sum(exponentiated)
-        return [np.exp(-self.gamma * i) / total for i in exponentiated]
+        return [i / total for i in exponentiated]
 
 
 class DDREnvIterative(DDREnvSoftmin):
@@ -309,6 +319,7 @@ class DDREnvIterative(DDREnvSoftmin):
     generalisation after learning. i.e. each step is to only get the value for
     one edge which is selected in the observation at the start of the step.
     """
+    # TODO: try non-softmin version too (i.e. dest-based)
 
     def __init__(self,
                  dm_generator_getter: Callable[
@@ -316,20 +327,16 @@ class DDREnvIterative(DDREnvSoftmin):
                      Generator[Demand, None, None]],
                  dm_memory_length: int,
                  graph: nx.DiGraph,
-                 gamma: float = 2):
-        super().__init__(dm_generator_getter, dm_memory_length, graph, gamma)
+                 **kwargs):
+        super().__init__(dm_generator_getter, dm_memory_length, graph, **kwargs)
 
         # Precompute list of flows for use in routing translation
         num_nodes = self.graph.number_of_nodes()
-        self.flows = [(i, j) for i in range(num_nodes) for j in range(num_nodes)
-                      if i != j]
-        self.gamma = gamma
 
         self.action_space = gym.spaces.Box(
-            low=0.0,
-            high=1.0,
+            low=-np.inf,
+            high=np.inf,
             shape=(1,),
-            # TODO: Although try later with number of lows for non softmin version
             dtype=np.float64)
         self.observation_space = gym.spaces.Box(
             low=-np.inf,
@@ -373,19 +380,17 @@ class DDREnvIterative(DDREnvSoftmin):
         self.edge_values[edge_idx] = action[0]
         self.edge_set[edge_idx] = 1
 
-        # TODO: as rewards are generally negative, this is currently a big
-        #  issue. ask Kai advice
-        reward = 0
+        routing = self.get_routing(self.edge_values)
+        reward = self.get_reward(routing)
 
         # iteration start: update dm and shuffle the edge order
         #                  also calc prev routing and give reward
         # TODO: see how it performs without the shuffle
         if self.iter_idx == 0:
-            routing = self.get_routing(self.edge_values)
-            reward = self.get_reward(routing)
             self.edge_set = np.zeros(self.graph.number_of_edges(), dtype=float)
-            self.edge_values = np.zeros(self.graph.number_of_edges(),
-                                        dtype=float)
+            # TODO: try with and without zeroing this
+            # self.edge_values = np.zeros(self.graph.number_of_edges(),
+            #                             dtype=float)
 
             new_dm = next(self.dm_generator, None)
             if new_dm is None:
@@ -433,15 +438,16 @@ class DDREnvIterative(DDREnvSoftmin):
 
     def get_data_dict(self) -> Dict:
         """
-        Gets a data dict to return in the step function. Contains the
-        utilisation under oblivious routing
+        Gets a data dict to return in the step function.
         Returns:
-            dict with utilisation under oblivious routing
+            dict with oblivious routing from parent and information about
+            iteration state
         """
         target_edge_idx = self.edge_order[self.iter_idx]
         target_edge = np.identity(self.graph.number_of_edges())[
                       target_edge_idx:target_edge_idx + 1]
-        data_dict = {'oblivious_utilisation': max_link_utilisation.calc(
-            self.graph, self.dm_memory[0], self.oblivious_routing),
-            'iter_idx': self.iter_idx, 'target_edge': target_edge, 'edge_set': self.edge_set, 'values': self.edge_values}
+        data_dict = super().get_data_dict()
+        data_dict.update({'iter_idx': self.iter_idx, 'target_edge': target_edge,
+                          'edge_set': self.edge_set,
+                          'values': self.edge_values})
         return data_dict

gym-ddr/gym_ddr/envs/max_link_utilisation.py
@@ -1,11 +1,11 @@
-from typing import Type
+from typing import Tuple
 
-import numpy as np
 import networkx as nx
+import numpy as np
 from ortools.linear_solver import pywraplp
 
-Demand = Type[np.ndarray]
-Routing = Type[np.ndarray]
+Demand = np.ndarray
+Routing = np.ndarray
 
 
 def opt(graph: nx.DiGraph, demands: Demand) -> float:
@@ -99,9 +99,11 @@ def opt(graph: nx.DiGraph, demands: Demand) -> float:
         constraint_i = solver.Constraint(1, 1, '(4,{})'.format(i))
         for edge_dest in list(sorted(graph.adj[commodity[1]].keys())):
             constraint_i.SetCoefficient(
-                flow_variables[i][edge_index_dict[(edge_dest, commodity[1])]], 1)
+                flow_variables[i][edge_index_dict[(edge_dest, commodity[1])]],
+                1)
             constraint_i.SetCoefficient(
-                flow_variables[i][edge_index_dict[(commodity[1], edge_dest)]], -1)
+                flow_variables[i][edge_index_dict[(commodity[1], edge_dest)]],
+                -1)
         conservation_dest_constraints.append(constraint_i)
 
     ## OBJECTIVES
@@ -130,6 +132,101 @@ def opt(graph: nx.DiGraph, demands: Demand) -> float:
 
     return objective.Value()
 
+"""
+Algorithm Planning
+end result: array of bandwidth used on each edge
+high-level: calculate such an array for every flow and sum them
+per-flow:
+  1. initialise entire demand at start node
+  2. push bandwidth onto edges and neighbour nodes (delete from self)
+  3. perform 2 for all nodes what were changed in prev step
+  3.1 NB: if dst node, then just absorb the flow rather than push on
+  4. end when no node changes value (may need some min float limit)
+"""
+
+
+def calc_per_flow_link_utilisation(graph: nx.DiGraph, flow: Tuple[int, int],
+                                   demand: float,
+                                   routing: np.ndarray) -> np.ndarray:
+    """
+    Calculates the link utilisation over a graph for a particular flow and its
+    demand. (NB utilisation in bandwidth, not relative to capacity)
+    Args:
+        graph: network graph to calculate over
+        flow: the source and destination node ids of the flow
+        demand: the bandwidth to push from source to sink
+        routing: edge splitting ratios to be used when routing
+    Returns:
+        Link utilisation as an ndarray (one value per edge)
+    """
+    # TODO: make this code run much faster
+    num_edges = graph.number_of_edges()
+    num_nodes = graph.number_of_nodes()
+    edge_mapping = {edge: i for i, edge in enumerate(sorted(graph.edges))}
+
+    link_utilisation = np.zeros(num_edges)
+    node_flow = np.zeros(num_nodes)  # the flow stored at a node
+    node_flow[flow[0]] = demand
+
+    to_explore = [flow[0]]
+    while to_explore:
+        current_node = to_explore.pop(0)
+        current_flow = node_flow[current_node]
+
+        # nothing to do here then so don't even try
+        if np.isclose(current_flow, 0.0):  # TODO: fix this to be better
+            continue
+
+        # this is the flow destination node so we absorb all flow
+        if current_node == flow[1]:
+            node_flow[current_node] = 0.0
+            continue
+
+        # push the flow at this node over all edges
+        for edge in graph.out_edges(current_node):
+            edge_index = edge_mapping[edge]
+            ratio = routing[edge_index]
+            node_flow[edge[1]] += ratio * current_flow
+            # all important step, update our output
+            link_utilisation[edge_index] += ratio * current_flow
+            # have updated the dst so need to add it to the list of things to do
+            to_explore.append(edge[1])
+        # we've moved all the flow from this node now, so reset back to zero
+        node_flow[current_node] = 0.0
+
+    return link_utilisation
+
+
+def calc_overall_link_utilisation(graph: nx.DiGraph, demands: Demand,
+                                  routing: Routing) -> np.ndarray:
+    """
+    Calculates the overall utilisation of each link in a network given a routing
+    choice and a set of demands. (NB utilisation in bandwidth, not relative to
+    capacity)
+    Args:
+        graph: network graph to calculate over
+        demands: demands matrix, format is: 1 x (|V|*(|V|-1)) (ordering is
+                 (0,1),(0,2),...,(1,0),(1,2),... etc)
+        routing: per-flow edge splitting rations, format is (|V|*(|V|-1)) x |E|
+                 (same ordering as flows, edges numerically ordered)
+    Returns:
+        Link utilisation as an ndarray (one value per edge)
+    """
+    num_edges = graph.number_of_edges()
+    num_nodes = graph.number_of_nodes()
+    flows = [(i, j) for i in range(num_nodes) for j in range(num_nodes) if
+             i != j]
+
+    link_utilisation = np.zeros(num_edges)
+
+    for i, flow in enumerate(flows):
+        flow_link_utilisation = calc_per_flow_link_utilisation(graph, flow,
+                                                               demands[i],
+                                                               routing[i])
+        link_utilisation += flow_link_utilisation
+
+    return link_utilisation
+
 
 def calc(graph: nx.DiGraph, demands: Demand, routing: Routing) -> float:
     """
@@ -144,31 +241,10 @@ def calc(graph: nx.DiGraph, demands: Demand, routing: Routing) -> float:
 
     NB: does not actually check given routing is valid but assumes it is.
     """
-    max_link_utilisation = 0.0
-    edges = sorted(graph.edges())
+    edge_capacities = [e[2]['weight'] for e in sorted(graph.edges(data=True))]
+    link_utilisation = calc_overall_link_utilisation(graph, demands, routing)
+    # Because utilisation compared to link width is what we care about here
+    ratio_capacities = np.divide(link_utilisation, edge_capacities)
 
-    for i, edge in enumerate(edges):
-        link_utilisation = 0.0
-        # This loops over each flow
-        for j in range(routing.shape[0]):
-            link_utilisation += (routing[j][i] * demands[j])
-        max_link_utilisation = max(
-            max_link_utilisation,
-            link_utilisation / graph.get_edge_data(*edge)['weight'])
-    return max_link_utilisation
+    return np.max(ratio_capacities)
 
-
-def racke_routing(graph: nx.DiGraph) -> Routing:
-    """
-    Builds an efficient oblivious routing for the graph using the method
-    presented by Racke
-    Args:
-        graph: graph to produce routing for
-
-    Returns:
-        A routing as an an np array
-    """
-    # TODO: implement
-    return np.zeros(
-        (graph.number_of_nodes() * (graph.number_of_nodes()-1),
-         graph.number_of_edges()))