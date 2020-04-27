ddf470ff5ffb58da2b03c115cb109a8e0263d756
Oliver Hope <oliverdnhope@gmail.com>
2020-04-27 11:40:31 +0100

    make iterative use vectorization and normalization

--- experiments/ddr_gnn_iterative.py
+++ experiments/ddr_gnn_iterative.py
@@ -1,47 +1,65 @@
 import tensorflow as tf
+from stable_baselines.common.vec_env import SubprocVecEnv, VecNormalize
 
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 import gym
 import gym_ddr.envs.demand_matrices as dm
+from gym_ddr.envs.max_link_utilisation import MaxLinkUtilisation
 import numpy as np
-from ddr_learning_helpers import graphs
+from ddr_learning_helpers import graphs, yates
 from stable_baselines import PPO2
 from stable_baselines_ddr.gnn_policy import GnnDdrIterativePolicy
 
-# load/generate graph
-#graph = graphs.topologyzoo("TLex", 10000)
-graph = graphs.basic()
+if __name__ == "__main__":
+    # load/generate graph
+    # graph = graphs.topologyzoo("TLex", 10000)
+    graph = graphs.basic()
 
-# set env parameters
-rs = np.random.RandomState()
-dm_memory_length = 10
-num_demands = graph.number_of_nodes() * (graph.number_of_nodes() - 1)
-nn = graph.number_of_nodes()
-lend = len([(i, j) for i in range(nn) for j in range(nn) if i != j])
-num_edges = graph.number_of_edges()
-# dm_generator_getter = lambda: dm.cyclical_sequence(
-#     lambda: dm.bimodal_demand(num_demands, rs), 40, 5, 0.0, rs)
-dm_generator_getter = lambda: dm.average_sequence(
-    lambda: dm.gravity_demand(graph), 40, 5, 0.4, rs)
+    ## ENV PARAMETERS
+    rs = np.random.RandomState()  # Random state
+    dm_memory_length = 10  # Length of memory of dms in each observation
+    num_demands = graph.number_of_nodes() * (graph.number_of_nodes() - 1)  # Demand matrix size (dependent on graph size
+    dm_generator_getter = lambda seed: dm.cyclical_sequence(  # A function that returns a generator for a sequence of demands
+        lambda rs_l: dm.bimodal_demand(num_demands, rs_l), 20, 1, 0.0, seed=seed)
+    # demand_sequences = [list(dm_generator_getter()) for i in range(2)]  # Collect the generator into a sequence
+    mlu = MaxLinkUtilisation(graph)  # Friendly max link utilisation class
+    demand_sequences = map(dm_generator_getter, [32, 32])
+    demands_with_opt = [[(demand, mlu.opt(demand)) for demand in sequence] for  # Merge opt calculations into the demand sequence
+                        sequence in demand_sequences]
 
-# make env
-env = gym.make('ddr-iterative-v0', dm_generator_getter=dm_generator_getter,
-               dm_memory_length=dm_memory_length, graph=graph)
+    oblivious_routing = None  # yates.get_oblivious_routing(graph)
 
-# make model
-model = PPO2(GnnDdrIterativePolicy, env, verbose=1,
-             policy_kwargs={'network_graph': graph,
-                            'dm_memory_length': dm_memory_length},
-             tensorboard_log="./gnn_tensorboard/")
+    # make env
+    env = lambda: gym.make('ddr-iterative-v0',
+                           dm_sequence=demands_with_opt,
+                           dm_memory_length=dm_memory_length,
+                           graph=graph,
+                           oblivious_routing=oblivious_routing)
 
-# learn
-model.learn(total_timesteps=100000)
+    vec_env = SubprocVecEnv([env, env, env, env])
+    # Try with and without. May interfere with iter
+    normalised_env = VecNormalize(vec_env, training=True, norm_obs=True,
+                                  norm_reward=False)
 
-# use
-obs = env.reset()
-for i in range(1000):
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    print(rewards)
-    env.render()
+    # make model
+    model = PPO2(GnnDdrIterativePolicy,
+                 normalised_env,
+                 verbose=1,
+                 policy_kwargs={'network_graph': graph,
+                                'dm_memory_length': dm_memory_length,
+                                'vf_arch': "graph",
+                                },
+                 tensorboard_log="./gnn_tensorboard/")
+
+    # learn
+    model.learn(total_timesteps=20000, tb_log_name="gnn_iter_softmin_basic")
+    model.save("model_gnn_iter_softmin_basic")
+
+    # use
+    obs = normalised_env.reset()
+    for i in range(11):
+        action, _states = model.predict(obs)
+        obs, rewards, dones, info = normalised_env.step(action)
+        print(rewards)
+        print(info)
