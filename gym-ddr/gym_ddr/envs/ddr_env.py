change dm memory observation to just in and out values, change scaling of edge weights to use epsilon and fix iteration initialisation values

gym-ddr/gym_ddr/envs/ddr_env.py
@@ -11,6 +11,8 @@ Demand = Tuple[np.ndarray, float]
 Observation = np.ndarray
 Action = np.ndarray
 
+EPSILON = 1e-5
+
 
 def normalise_array(array: np.ndarray) -> np.ndarray:
     """
@@ -28,7 +30,7 @@ class DDREnv(gym.Env):
     """
     Gym env for data driven routing
 
-    Observations are: last k routing and DMs
+    Observations are: sum of in and out demand for each node
     Actions are: a routing
 
     Actions are fully specified routing (i.e. splitting ratio for each flow over
@@ -68,14 +70,18 @@ class DDREnv(gym.Env):
         self.observation_space = gym.spaces.Box(
             low=0,
             high=np.inf,
-            shape=(dm_memory_length * graph.number_of_nodes() *
-                   (graph.number_of_nodes() - 1),),
+            shape=(dm_memory_length * graph.number_of_nodes() * 2,),
             dtype=np.float64)
 
         # so that we can make the input routing valid
         self.out_edge_count = np.cumsum([i[1] for i in
                                          sorted(graph.out_degree(),
                                                 key=lambda x: x[0])])
+
+        # Precompute list of flows for use in routing and dm translation
+        num_nodes = self.graph.number_of_nodes()
+        self.flows = [(i, j) for i in range(num_nodes) for j in range(num_nodes)
+                      if i != j]
 
         self.oblivious_routing = oblivious_routing
 
@@ -102,12 +108,8 @@ class DDREnv(gym.Env):
         self.dm_index += 1
         if self.dm_index == len(self.dm_sequence[self.dm_sequence_index]):
             self.done = True
-            # Move to the next dm sequence (choose randomly)
-            self.dm_sequence_index = np.random.randint(0, len(self.dm_sequence))
-            # Move to the next dm sequence (iterate)
-            # self.dm_sequence_index = (self.dm_sequence_index + 1) % len(
-            #     self.dm_sequence)
-            return self.get_observation(), 0.0, self.done, dict()
+            reward = 0.0
+            data_dict = {}
         else:
             new_dm = self.dm_sequence[self.dm_sequence_index][self.dm_index]
             self.dm_memory.append(new_dm)
@@ -119,9 +121,14 @@ class DDREnv(gym.Env):
         return self.get_observation(), reward, self.done, data_dict
 
     def reset(self) -> Observation:
+        # first choose a random dm sequence
+        self.dm_sequence_index = np.random.randint(0, len(self.dm_sequence))
+        # tne place us at the start of that sequence
         self.dm_index = 0
+        # pre-fill the memory from the sequence
         self.dm_memory = [self.dm_sequence[self.dm_sequence_index][i] for i in
                           range(self.dm_memory_length)]
+        # move the index to the first element after the memory
         self.dm_index += self.dm_memory_length
         self.done = False
         return self.get_observation()
@@ -169,12 +176,19 @@ class DDREnv(gym.Env):
 
     def get_observation(self) -> Observation:
         """
-        Flattens observation for input to learners
+        Takes the dms and presents a history of outgoing and incoming demands
+        per node (flattened)
         Returns:
-            A flat np array of the demand matrix
+            A flat np array of the demands
         """
-        dm_no_opt_memory = [dm[0] for dm in self.dm_memory]
-        return np.stack(dm_no_opt_memory).ravel()
+        node_demands_memory = []
+        for dm in self.dm_memory:
+            node_demands = np.zeros(self.graph.number_of_nodes() * 2, dtype=float)
+            for flow_idx, (src, dst) in enumerate(self.flows):
+                node_demands[src * 2] += dm[0][flow_idx]
+                node_demands[(dst * 2) + 1] += dm[0][flow_idx]
+            node_demands_memory.append(node_demands)
+        return np.stack(node_demands_memory).ravel()
 
     def get_data_dict(self) -> Dict:
         """
@@ -212,11 +226,6 @@ class DDREnvDest(DDREnv):
             shape=(
             self.graph.number_of_nodes() * self.graph.number_of_edges(),),
             dtype=np.float64)
-
-        # Precompute list of flows for use in routing translation
-        num_nodes = self.graph.number_of_nodes()
-        self.flows = [(i, j) for i in range(num_nodes) for j in range(num_nodes)
-                      if i != j]
 
         # Indices of the edges for lookup in routing translation
         # Takes each edge of graph to an index under its source node
@@ -296,7 +305,7 @@ class DDREnvSoftmin(DDREnv):
         Converts a softmin routing to full routing and prunes cycles with a
         very basic heuristic
         Args:
-            action: dims 0: edges
+            action: dims 0: edges, final value is gamma
         Returns:
             A fully specified routing (dims 0: flows, 1: edges)
         """
@@ -305,11 +314,16 @@ class DDREnvSoftmin(DDREnv):
 
         # TODO: revisit
         # First we place the routing edge weights on the graph (and rescale
-        # between 1 and 2 so that no edges have 0 weight which causes pruning
-        # issues when removing cycles)
+        # between 0 and 1 but plus epsilon so that no edges have 0 weight which
+        # causes pruning issues when removing cycles)
         for i, edge in enumerate(sorted(self.graph.edges)):
             self.graph[edge[0]][edge[1]]['route_weight'] = (
-                    (action[i] + 3.0) / 2.0)
+                    (action[i] + 1.0) / 2.0) + EPSILON
+
+        # TODO: revisit
+        # rescale softmin gamma from action range [-1, 1] to [0, e^2]
+        # is not linear because action of gamma is not linear
+        gamma = np.exp(action[-1] + 1.0) - 1.0
 
         # then for each flow we calculate the splitting ratios
         for flow_idx, flow in enumerate(self.flows):
@@ -340,7 +354,7 @@ class DDREnvSoftmin(DDREnv):
                         pruned_graph[out_edge[0]][out_edge[1]]['route_weight'] + \
                         distances[out_edge[1]]
                 # softmin the out_edge weights so that ratios sum to one
-                softmin_weights = self.softmin(out_edge_weights)
+                softmin_weights = self.softmin(out_edge_weights, gamma)
                 # assign to the splitting ratios for this node and flow to
                 # overall routing
                 for out_edge_idx, weight in enumerate(softmin_weights):
@@ -348,15 +362,16 @@ class DDREnvSoftmin(DDREnv):
                         self.edge_index_map[out_edges[out_edge_idx]]] = weight
         return full_routing
 
-    def softmin(self, array: np.ndarray) -> np.ndarray:
+    def softmin(self, array: np.ndarray, gamma: float) -> np.ndarray:
         """
         Calculates and returns the softmin of an np array
         Args:
             array: an np array
+            gamma: scaling value for softmin (must be positive)
         Returns:
             np array the same size but softminned
         """
-        exponentiated = np.exp(np.multiply(array, -self.gamma))
+        exponentiated = np.exp(np.multiply(array, -gamma))
         total = sum(exponentiated)
         return np.divide(exponentiated, total)
 
@@ -385,8 +400,7 @@ class DDREnvIterative(DDREnvSoftmin):
             low=0.0,
             high=np.inf,
             # Space contains per edge to set and already set
-            shape=(dm_memory_length * graph.number_of_nodes() *
-                   (graph.number_of_nodes() - 1) +
+            shape=(dm_memory_length * graph.number_of_nodes() * 2 +
                    (3 * graph.number_of_edges()),),
             dtype=np.float64)
 
@@ -438,52 +452,61 @@ class DDREnvIterative(DDREnvSoftmin):
 
         # iteration start: update dm and shuffle the edge order
         #                  also calc prev routing and give reward
-        # TODO: see how it performs without the shuffle
         if self.iter_idx == 0:
+            # reset array of which edges have been set
             self.edge_set = np.zeros(self.graph.number_of_edges(), dtype=float)
+            # change order in which we ask for edges to be set
+            np.random.shuffle(self.edge_order)
             # Set to midvalue at start so algorithm can change each edge to be
             # more or less favourable
             self.edge_values = np.zeros(self.graph.number_of_edges(), dtype=float)
+            # move forwards to next dm in this sequence
             self.dm_index += 1
-            # TODO: assess is this is correct way and place to reset last reward (and can we improve?)
             self.iteration_reward = self.last_reward  # save the reward for the iteration for debugging
-            self.last_reward = 0.0
+
             if self.dm_index == len(self.dm_sequence[self.dm_sequence_index]):
+                # at end of dm sequence so end episode
                 self.done = True
-                # Move to next dm sequence (randomly)
-                self.dm_sequence_index = np.random.randint(0, len(
-                    self.dm_sequence))
-                # Move to next dm sequence (iterate)
-                # self.dm_sequence_index = (self.dm_sequence_index + 1) % len(
-                #     self.dm_sequence)
             else:
+                # still in dm sequence so update the memory by appending new dm
                 new_dm = self.dm_sequence[self.dm_sequence_index][self.dm_index]
                 self.dm_memory.append(new_dm)
                 if len(self.dm_memory) > self.dm_memory_length:
                     self.dm_memory.pop(0)
-            np.random.shuffle(self.edge_order)
-        # inside an iteration so just change the index
+                # initialise reward to that given by initial edge values routing
+                routing = self.get_routing(self.edge_values)
+                self.last_reward = self.get_reward(routing)
+
         self.iter_idx = (self.iter_idx + 1) % self.iter_length
 
         data_dict = self.get_data_dict()
-
         return self.get_observation(), comparison_reward, self.done, data_dict
 
     def reset(self) -> Observation:
+        # first choose a random dm sequence
+        self.dm_sequence_index = np.random.randint(0, len(self.dm_sequence))
+        # then place us at the start of that sequence
         self.dm_index = 0
+        # pre-fill the memory from the sequence
         self.dm_memory = [self.dm_sequence[self.dm_sequence_index][i] for i in
                           range(self.dm_memory_length)]
+        # move the index to the first element after the memory
         self.dm_index += self.dm_memory_length
         self.done = False
 
-        # iteration variables
+        ## iteration variables
         self.iter_length = self.graph.number_of_edges()
+        # the order in which we want to set the edges (it is shuffled for
+        # generalisation)
         self.edge_order = np.arange(self.graph.number_of_edges())
         np.random.shuffle(self.edge_order)
         self.iter_idx = 0
         self.edge_set = np.zeros(self.graph.number_of_edges(), dtype=float)
+        # intial edge values are midvalues of the action space
         self.edge_values = np.zeros(self.graph.number_of_edges(), dtype=float)
-        self.last_reward = 0.0
+        # initialise reward to that given by initial edge values routing
+        routing = self.get_routing(self.edge_values)
+        self.last_reward = self.get_reward(routing)
         return self.get_observation()
 
     def get_observation(self) -> Observation:
@@ -492,7 +515,6 @@ class DDREnvIterative(DDREnvSoftmin):
         Returns:
             A flat np array of the demand matrix
         """
-        # TODO: make current edge value part of observation
         target_edge_idx = self.edge_order[self.iter_idx]
         target_edge = np.identity(self.graph.number_of_edges())[
                       target_edge_idx:target_edge_idx + 1]
@@ -501,8 +523,8 @@ class DDREnvIterative(DDREnvSoftmin):
         iter_info[0::3] = self.edge_set  # TODO: maybe get rid of this?
         iter_info[1::3] = target_edge
         iter_info[2::3] = self.edge_values  # TODO: maybe don't do this as these are unscaled
-        dm_no_opt_memory = [dm[0] for dm in self.dm_memory]
-        return np.concatenate((np.stack(dm_no_opt_memory).ravel(), iter_info))
+        demands_history = DDREnv.get_observation(self)
+        return np.concatenate((demands_history, iter_info))
 
     def get_data_dict(self) -> Dict:
         """

stable-baselines-ddr/stable_baselines_ddr/gnn_policy.py
@@ -78,10 +78,7 @@ def vf_builder(vf_arch: str, graph: nx.DiGraph, latent: tf.Tensor,
                                      tf.constant([-1, num_nodes], np.int32))
         output_globals_vf = tf.reshape(shared_graphs[-1].globals,
                                        tf.constant([-1, 1], np.int32))
-        latent_vf = act_fun(
-            linear(
-                tf.concat([output_edges_vf, output_nodes_vf, output_globals_vf],
-                          1), "vf_fc0", 128, init_scale=np.sqrt(2)))
+        latent_vf = tf.concat([output_edges_vf, output_nodes_vf, output_globals_vf], 1)
     elif vf_arch == "graph":
         model_vf = EncodeProcessDecode(edge_output_size=1, node_output_size=1)
         output_graphs_vf = model_vf(input_graph, iterations)
@@ -89,9 +86,7 @@ def vf_builder(vf_arch: str, graph: nx.DiGraph, latent: tf.Tensor,
                                      tf.constant([-1, num_edges], np.int32))
         output_nodes_vf = tf.reshape(output_graphs_vf[-1].nodes,
                                      tf.constant([-1, num_nodes], np.int32))
-        latent_vf = act_fun(
-            linear(tf.concat([output_edges_vf, output_nodes_vf], 1), "vf_fc0",
-                   128, init_scale=np.sqrt(2)))
+        latent_vf = tf.concat([output_edges_vf, output_nodes_vf], 1)
     elif vf_arch == "mlp":
         latent_vf = latent
         latent_vf = act_fun(
@@ -128,7 +123,7 @@ def gnn_extractor(flat_observations: tf.Tensor, act_fun: tf.function,
     num_batches = tf.shape(latent)[0]
 
     # slice the data dimension to split the edge and node features
-    node_features = tf.reshape(latent, [-1, (num_nodes - 1) * dm_memory_length],
+    node_features = tf.reshape(latent, [-1, 2 * dm_memory_length],
                                name="node_feat_input")
 
     # initialise unused input features to all zeros
@@ -168,7 +163,9 @@ def gnn_extractor(flat_observations: tf.Tensor, act_fun: tf.function,
     #     unknown dims
     output_edges = tf.reshape(output_graphs[-1].edges,
                               tf.constant([-1, num_edges], np.int32))
-    latent_policy_gnn = output_edges
+    output_globals = tf.reshape(output_graphs[-1].globals,
+                                tf.constant([-1, 1], np.int32))
+    latent_policy_gnn = tf.concat([output_edges, output_globals], axis=1)
 
     # build value function network
     latent_vf = vf_builder(vf_arch, network_graph, latent, act_fun,
@@ -202,13 +199,13 @@ def gnn_iter_extractor(flat_observations: tf.Tensor, act_fun: tf.function,
 
     # slice the data dimension to split the edge and node features
     node_features_slice = tf.slice(latent, [0, 0], [-1, num_nodes * (
-            num_nodes - 1) * dm_memory_length])
+            2) * dm_memory_length])
     edge_features_slice = tf.slice(latent, [0, num_nodes * (
-            num_nodes - 1) * dm_memory_length], [-1, -1])
+            2) * dm_memory_length], [-1, -1])
 
     # reshape node features to flat batches but still vector in dim 1 per node
     node_features = tf.reshape(node_features_slice,
-                               [-1, (num_nodes - 1) * dm_memory_length],
+                               [-1, 2 * dm_memory_length],
                                name="node_feat_input")
     # reshape edge features to flat batches but vector in dim 1 per edge
     edge_features = tf.reshape(edge_features_slice, [-1, 3],
@@ -246,10 +243,10 @@ def gnn_iter_extractor(flat_observations: tf.Tensor, act_fun: tf.function,
     # We still output other for use in shared part of value function
     # The global output is: [edge_value, gamma_value]
     model = EncodeProcessDecode(edge_output_size=1, node_output_size=1,
-                                global_output_size=1)
+                                global_output_size=2)
     output_graphs = model(input_graph, iterations)
     output_global = tf.reshape(output_graphs[-1].globals,
-                               tf.constant([-1, 1], np.int32))
+                               tf.constant([-1, 2], np.int32))
     latent_policy_gnn = output_global
 
     # build value function network
@@ -391,8 +388,23 @@ class FeedForwardPolicyWithGnn(ActorCriticPolicy):
             self._proba_distribution, self._policy, self.q_value = \
                 self.pdtype.proba_distribution_from_latent(pi_latent, vf_latent,
                                                            init_scale=0.01)
+            # TODO: get this working (whole change of output space size thing)
+                # self.proba_distribution_no_pi_linear(pi_latent, vf_latent,
+                #                                      init_scale=0.01)
 
         self._setup_init()
+
+    def proba_distribution_no_pi_linear(self, pi_latent_vector, vf_latent_vector, init_scale=0.01, init_bias=0.0):
+        """
+        Modification of the standard function without a linear layer on pi so
+        that size of action space of trained model can be varied
+        """
+        # mean = linear(pi_latent_vector, 'pi', self.size, init_scale=init_scale, init_bias=init_bias)
+        mean = pi_latent_vector  # need this one to be able to vary to change graph size
+        logstd = tf.get_variable(name='pi/logstd', shape=[1, self.pdtype.size], initializer=tf.zeros_initializer())
+        pdparam = tf.concat([mean, mean * 0.0 + logstd], axis=1)
+        q_values = linear(vf_latent_vector, 'q', self.pdtype.size, init_scale=init_scale, init_bias=init_bias)
+        return self.pdtype.proba_distribution_from_flat(pdparam), mean, q_values
 
     def step(self, obs, state=None, mask=None, deterministic=False):
         if deterministic: