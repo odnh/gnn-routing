d4ec04cb6732cc8413af5a4ad59b9feec600ded2
Oliver Hope <oliverdnhope@gmail.com>
2020-05-26 21:11:24 +0100

    add baseline across plots

--- evaluation/make_plots.py
+++ evaluation/make_plots.py
@@ -1,28 +1,11 @@
 """Helpful functions for reading results and plotting them"""
-
 import matplotlib
-import numpy as np
 import pandas as pd
 from jsonlines import jsonlines
 
 matplotlib.use('pgf')  # to play nice with custom lualatex stuff
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-
-def demo_plot():
-    """Demo of how to do a plot. Not related to project at all."""
-    t = np.linspace(0.0, 1.0, 100)
-    s = np.cos(4 * np.pi * t) + 2
-
-    fig, ax = plt.subplots(figsize=(6, 4), tight_layout=True)
-    ax.plot(t, s)
-
-    ax.set_xlabel(r'\textbf{time (s)}')
-    ax.set_ylabel('\\textit{Velocity (\N{DEGREE SIGN}/sec)}', fontsize=16)
-    ax.set_title(r'\TeX\ is Number $\displaystyle\sum_{n=1}^\infty'
-                 r'\frac{-e^{i\pi}}{2^n}$!', fontsize=16, color='r')
-    plt.savefig('figure.pgf')
 
 
 def read_results(test_number: str, policy_name: str) -> pd.DataFrame:
@@ -33,20 +16,20 @@ def read_results(test_number: str, policy_name: str) -> pd.DataFrame:
 
     df = pd.DataFrame(
         columns=['utilisation', 'opt_utilisation', 'oblivious_utilisation',
-                 'sequence_type', 'graph'])
+                 'sequence_type', 'graphs'])
     with jsonlines.open(path) as f:
         for result in f:
             sequence_type = result['sequence_type']
-            graph = result['graph']
+            graphs = '.'.join(result['graphs'])
             for i in range(len(result['utilisations'])):
-                df.append(
+                df = df.append(
                     {'utilisation': result['utilisations'][i],
                      'opt_utilisation': result['opt_utilisations'][i],
                      'oblivious_utilisation': result['oblivious_utilisations'][
                          i],
                      'sequence_type': sequence_type,
-                     'graph': graph
-                     })
+                     'graphs': graphs
+                     }, ignore_index=True)
 
     # to easily separate experiments in plots
     df['test_number'] = test_number
@@ -68,15 +51,20 @@ def plot_exp1():
     plt.clf()
     sns.boxplot(y='ratio', x='policy_name', data=df, palette='colorblind',
                 hue='test_number')
-    sns.lineplot(y='oblivious_ratio', x='policy_name', data=df,
-                 palette='colorblind', hue='test_number', markers=False)
+
+    # draw oblivious ratio lines
+    oblivious_heights = list(df.groupby('test_number')['oblivious_ratio'].mean())
+    palette = sns.color_palette("colorblind")
+    for i, height in enumerate(oblivious_heights):
+        plt.axhline(height, ls='--', color=palette[i])
+
     plt.savefig('plots/exp1.pgf')
 
 
 def plot_exp2():
     """Generate plots for exp2"""
     tests = [(test_number, policy_name) for test_number in
-             ['2.1', '2.2', '2.3', '2.4'] for
+             ['2.1', '2.2'] for
              policy_name in ['mlp', 'lstm', 'gnn', 'iter']]
     results = [read_results(*test) for test in tests]
     df = pd.concat(results)
@@ -84,8 +72,13 @@ def plot_exp2():
     plt.clf()
     sns.boxplot(y='ratio', x='policy_name', data=df, palette='colorblind',
                 hue='test_number')
-    sns.lineplot(y='oblivious_ratio', x='policy_name', data=df,
-                 palette='colorblind', hue='test_number', markers=False)
+
+    # draw oblivious ratio lines
+    oblivious_heights = list(df.groupby('test_number')['oblivious_ratio'].mean())
+    palette = sns.color_palette("colorblind")
+    for i, height in enumerate(oblivious_heights):
+        plt.axhline(height, ls='--', color=palette[i])
+
     plt.savefig('plots/exp2.pgf')
 
 
@@ -100,8 +93,13 @@ def plot_exp3():
     plt.clf()
     sns.boxplot(y='ratio', x='policy_name', data=df, palette='colorblind',
                 hue='test_number')
-    sns.lineplot(y='oblivious_ratio', x='policy_name', data=df,
-                 palette='colorblind', hue='test_number', markers=False)
+
+    # draw oblivious ratio lines
+    oblivious_heights = list(df.groupby('test_number')['oblivious_ratio'].mean())
+    palette = sns.color_palette("colorblind")
+    for i, height in enumerate(oblivious_heights):
+        plt.axhline(height, ls='--', color=palette[i])
+
     plt.savefig('plots/exp3.pgf')
 
 
@@ -115,8 +113,13 @@ def plot_exp4():
     plt.clf()
     sns.boxplot(y='ratio', x='policy_name', data=df, palette='colorblind',
                 hue='test_number')
-    sns.lineplot(y='oblivious_ratio', x='policy_name', data=df,
-                 palette='colorblind', hue='test_number', markers=False)
+
+    # draw oblivious ratio lines
+    oblivious_heights = list(df.groupby('test_number')['oblivious_ratio'].mean())
+    palette = sns.color_palette("colorblind")
+    for i, height in enumerate(oblivious_heights):
+        plt.axhline(height, ls='--', color=palette[i])
+
     plt.savefig('plots/exp4.pgf')
 
 
